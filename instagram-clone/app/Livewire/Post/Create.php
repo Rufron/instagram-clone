f21b018ee9b378e51a81bf91d0861b51f9ebc55b
Rufron <kipchumbabrian572@gmail.com>
2024-08-19 22:09:36 +0300

    fourteenth commit

--- instagram-clone/app/Livewire/Post/Create.php
+++ instagram-clone/app/Livewire/Post/Create.php
@@ -2,15 +2,33 @@
 
 namespace App\Livewire\Post;
 
-use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
+use Termwind\Components\Dd;
 
 class Create extends ModalComponent
 {
+    use WithFileUploads;
+
+    public $media=[];
+    public $description;
+    public $location;
+    public $hide_like_view;
+    public $allow_commenting;
 
     public static function modalMaxWidth(): string
     {
         return '4xl';
+    }
+
+    function submit() {
+        dd([
+            $this->media,
+            $this->description,
+            $this->location,
+            $this->hide_like_view,
+            $this->allow_commenting,
+        ]);
     }
 
     public function render()
